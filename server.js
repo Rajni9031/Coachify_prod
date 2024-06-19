bebc8f4ab16f7a829421bf316eeff643b8fe9462
Rajni Singh <rajnisingh9031@gmail.com>
2024-06-19 17:22:03 +0530

    New on vercel

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const path = require('path');
 
 dotenv.config();
 
@@ -11,7 +10,6 @@ const PORT = process.env.PORT || 7000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, './build')))
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
@@ -21,9 +19,6 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/profile', require('./routes/profile'));
 app.use('/api/batches', require('./routes/batch'));
 app.use('/api/student', require('./routes/student'));
-app.use("*", function (req, res) {
-  res.sendFile(path.join(__dirname,"./build/index.html"))
-})
 
 app.use((req, res, next) => {
   res.status(404).send('Route not found');
